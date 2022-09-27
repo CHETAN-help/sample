4a63aaaf058a6f9a87e07f4c49f5455b7d1d027f
CHETAN <79299515+CHETAN-help@users.noreply.github.com>
2022-09-27 18:18:05 +0530

    Add files via upload

---
+++ laxman.py
@@ -0,0 +1,18 @@
+a=int(input("Enter a number :"))
+
+if 0<a<11:
+    print("number between 1 to 10 ")
+
+elif 10<a<21 :
+    print(" 11 to 20")
+elif 20<a<31 :
+    print("21 to 30")
+    
+elif 30<a<41 :
+    print("31 to 40 ")
+
+elif a<0 :
+    print("less than 0 ")
+else :
+    print(" greater than 40")
+    
